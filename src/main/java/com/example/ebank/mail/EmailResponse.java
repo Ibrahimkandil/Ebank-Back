2bc77f96726f2a6bcbf5c8ba2208750c1b8f8841
brahim <ibrahim.kandil@istic.ucar.tn>
2024-06-21 04:51:44 +0200

    commit ibrahim kandil le 21/06/2024

---
+++ src/main/java/com/example/ebank/mail/EmailResponse.java
@@ -0,0 +1,21 @@
+package com.example.ebank.mail;
+
+public class EmailResponse {
+
+
+        private String response;
+
+
+        // Getters and setters
+        public void  setResponse(String resp) {
+            this.response=resp;
+        }
+
+
+        public String getResponse() {
+            return response;
+        }
+
+
+
+}
